b7e1de5ca84db10c3e571957fe38eb89ba067894
DomZem <dominik.zemlik.dev@gmail.com>
2024-10-12 19:14:48 +0200

    feat: configured typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,62 +1,87 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.tsx"],
+  darkMode: ["class"],
+  content: ["./src/**/*.tsx"],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: ["var(--font-geist-sans)", ...fontFamily.sans]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    fontSize: {
+      // 15px, 25px
+      base: [
+        "0.9375rem",
+        {
+          lineHeight: "1.5625rem",
+          fontWeight: "500",
+        },
+      ],
+      // 18px, 24px
+      lg: [
+        "1.125rem",
+        {
+          lineHeight: "1.5rem",
+          letterSpacing: "0.07em",
+          fontWeight: "700",
+        },
+      ],
+      // 24px, 33px
+      xl: [
+        "1.5rem",
+        {
+          lineHeight: "2.0625rem",
+          letterSpacing: "0.071em",
+          fontWeight: "700",
+        },
+      ],
+      // 28px, 38px
+      "2xl": [
+        "1.75rem",
+        {
+          lineHeight: "2.375rem",
+          letterSpacing: "0.071em",
+          fontWeight: "700",
+        },
+      ],
+      // 32px, 36px
+      "3xl": [
+        "2rem",
+        {
+          lineHeight: "2.25rem",
+          letterSpacing: "0.036em",
+          fontWeight: "700",
+        },
+      ],
+      // 40px, 44px
+      "4xl": [
+        "2.5rem",
+        { lineHeight: "2.75rem", letterSpacing: "0.0375em", fontWeight: "700" },
+      ],
+      // 56px, 58px
+      "5xl": [
+        "3.5rem",
+        {
+          lineHeight: "3.625rem",
+          letterSpacing: "0.0357em",
+          fontWeight: "700",
+        },
+      ],
+    },
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        destructive: "hsl(var(--destructive))",
+
+        background: "hsl(var(--background))",
+        border: "hsl(var(--border))",
+
+        muted: "hsl(var(--muted))",
+        accent: "hsl(var(--accent))",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
