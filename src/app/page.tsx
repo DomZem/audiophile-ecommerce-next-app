377abcc331aa21ec490d23401e89e81a28d29823
DomZem <dominik.zemlik.dev@gmail.com>
2024-10-12 18:03:52 +0200

    feat: initialized project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// import Link from "next/link";
+// import { getServerAuthSession } from "~/server/auth";
+// import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  // const session = await getServerAuthSession();
+
+  return <main>hello world</main>;
+}
