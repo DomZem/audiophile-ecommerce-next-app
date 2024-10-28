b813ffed9cfd94f70c9d28ffdceda2db4930330f
DomZem <dominik.zemlik.dev@gmail.com>
2024-10-28 21:29:41 +0100

    feat(api): added basic product router

---
+++ src/server/api/routers/product.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const productRouter = createTRPCRouter({
+  getAllByIds: publicProcedure
+    .input(
+      z.object({
+        ids: z.array(z.string()),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const products = await ctx.db.product.findMany({
+        where: {
+          id: {
+            in: input.ids,
+          },
+        },
+        select: {
+          id: true,
+          name: true,
+          card_image_url: true,
+          price_cents: true,
+          quantity: true,
+        },
+      });
+
+      return products;
+    }),
+});
