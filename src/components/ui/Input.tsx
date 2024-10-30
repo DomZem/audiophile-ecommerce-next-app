c8860d360fa24bf86321907524745e5fe082f2f1
DomZem <dominik.zemlik.dev@gmail.com>
2024-10-30 22:11:04 +0100

    feat: added input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "~/lib/utils";
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "focus-visible:ring-ring flex h-14 w-full rounded-lg border bg-white px-6 py-[18px] text-[0.875rem] font-bold text-black ring-offset-background placeholder:text-black/40 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
